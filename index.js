7d333675ed81d243ede8d94736e0086e7edd7f93
olgaklim29 <79910695+olgaklim29@users.noreply.github.com>
2021-08-28 19:48:29 +0200

    final1

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const colors = [
-    "#FFFFFF",
-    "#2196F3",
-    "#4CAF50",
-    "#FF9800",
-    "#009688",
-    "#795548",
-];
-const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-const refs = {
-    startBtn: document.querySelector('[data-action="start"]'),
-    stopBtn: document.querySelector('[data-action="stop"]'),
-    body: document.body,
-};
-let idInterval = null;
-refs.startBtn.addEventListener("click", onStart);
-refs.stopBtn.addEventListener("click", onStop);
-function onStart() {
-    console.log("Клик старт!");
-    idInterval = setInterval(() => {
-        refs.body.style.backgroundColor = randomColor();
+class CountdownTimer {
+    constructor(targetDate) {
+        this.targetDate = targetDate;
+        this.idInterval = null;
+    }
 
-    }, 1000);
-    refs.startBtn.disabled = true;
+    start() {
+        this.idInterval = setInterval(() => {
+            this.updateTimer(this.getRefs());
+        }, 1000);
+    }
+
+    getRefs() {
+        const container = document.querySelector("#timer-1");
+        const daysRef = container.querySelector('[data-value="days"]');
+        const hoursRef = container.querySelector('[data-value="hours"]');
+        const minsRef = container.querySelector('[data-value="mins"]');
+        const secsRef = container.querySelector('[data-value="secs"]');
+        return { container, daysRef, hoursRef, minsRef, secsRef };
+    }
+
+    updateTimer({ container, daysRef, hoursRef, minsRef, secsRef }) {
+        const time = this.targetDate - Date.now();
+        if (time <= 0) {
+            clearInterval(this.idInterval);
+            container.innerHTML = '<h1> Time is over </h1>';
+            return;
+        }
+        daysRef.textContent = Math.floor(time / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
+        hoursRef.textContent = Math.floor(
+            (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        )
+            .toString()
+            .padStart(2, "0");
+        minsRef.textContent = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+            .toString()
+            .padStart(2, "0");
+        secsRef.textContent = Math.floor((time % (1000 * 60)) / 1000)
+            .toString()
+            .padStart(2, "0");
+    }
 }
-function onStop() {
-    console.log("Клик стоп!");
-    clearInterval(idInterval);
-    refs.startBtn.disabled = false;
-}
-function randomColor() {
-    return `rgb(${randomIntegerFromInterval(0, 255)}, 
-    ${randomIntegerFromInterval(0, 255)}, ${randomIntegerFromInterval(0, 255)})`;
-}
+
+const timer = new CountdownTimer(new Date("Aug 15, 2022 23:50:00"));
+
+timer.start();
